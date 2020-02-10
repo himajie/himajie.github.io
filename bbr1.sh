8017be606717fcf582146fee9f5af22eb99a9bb7
small crab <about.ayur@gmail.com>
2020-02-10 16:29:15 +0800

    c
    c

---
+++ bbr1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+clear
+echo
+echo "#############################################################"
+echo "#                     google bbr                            #"
+echo "#############################################################"
+
+
+uname -r &&
+
+sudo rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org &&
+
+sudo rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm &&
+
+sudo yum --enablerepo=elrepo-kernel install kernel-ml -y &&
+
+rpm -qa | grep kernel &&
+
+sudo egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \' &&
+
+sudo grub2-set-default 1
+
