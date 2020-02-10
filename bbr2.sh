e34fd505588b537f8c200a7679def131ea6a418a
small crab <about.ayur@gmail.com>
2020-02-10 16:51:49 +0800

    c
    c

---
+++ bbr2.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+uname -r &&
+
+echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf &&
+
+echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf &&
+
+sudo sysctl -p &&
+
+sudo sysctl net.ipv4.tcp_available_congestion_control &&
+
+sudo sysctl -n net.ipv4.tcp_congestion_control &&
+
+lsmod | grep bbr &&
